bfbe24fee7d0cd840a20d9c8d39111c9c3ae9d1d
Krecik <Marcin.Jarzebski94@gmail.com>
2018-05-02 04:59:22 +0200

    added statistics sorting

--- GIS/simulator.py
+++ GIS/simulator.py
@@ -18,10 +18,14 @@ def main():
         time.sleep(0.01)
 
     # Print statistics
-    for airport in list(configuration.airports.values()):
+    airports_to_print = list(configuration.airports.values())
+    airports_to_print.sort(key= lambda x: x.times_visited)
+    for airport in airports_to_print:
         print('{name}: {times_visited}'.format(name=airport.name, times_visited=airport.times_visited))
 
-    for connection in configuration.connections:
+    connections_to_print = configuration.connections
+    connections_to_print.sort(key=lambda x: x.times_used)
+    for connection in connections_to_print:
         print('{beginning} -> {end} : {times_used}'.format(beginning=connection.beginning.name,
                                                            end=connection.end.name,
                                                            times_used=connection.times_used))
