ae623d88b519b5ae1d36342274a4d6fed632d05b
wolframowy <wolframowy@o2.pl>
2018-05-12 20:25:07 +0200

    Basic query by tag

--- SPDB/init.py
+++ SPDB/init.py
@@ -1,3 +1,9 @@
 import osmapi
+import urllib.request
+
 api = osmapi.OsmApi()
-print(api.NodeGet(123))
+#print(api.NotesSearch(123))
+nodeToSearch = "node[\"name:en\"=\"Warsaw\"];out;"
+contents = urllib.request.urlopen("http://overpass-api.de/api/interpreter?data=" + nodeToSearch).read()
+
+print(contents)
