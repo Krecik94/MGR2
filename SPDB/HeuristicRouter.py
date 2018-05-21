9986545a0a828c1957119e90e0240a9b4460bf94
Krecik <Marcin.Jarzebski94@gmail.com>
2018-05-22 01:51:29 +0200

    distance calculator

--- SPDB/HeuristicRouter.py
+++ SPDB/HeuristicRouter.py
@@ -1,3 +1,6 @@
+from math import sin, cos, sqrt, atan2, radians
+from geopy import distance
+
 class Route:
     def __init__(self, start, end, intermediate_points=[]):
         self.start = start
@@ -11,6 +14,28 @@ class Location:
         self.y = latitude
         self.xml_data = xml_data
         self.node_id = node_id
+
+
+def calculate_distance(input_longitide_1,
+                       input_latitude_1,
+                       input_longitide_2,
+                       input_latitude_2):
+    # approximate radius of earth in km
+    # R = 6373.0
+    #
+    # longitude_1 = radians(input_longitide_1)
+    # latitude_1 = radians(input_latitude_1)
+    # longitude_2 = radians(input_longitide_2)
+    # latitude_2 = radians(input_latitude_2)
+    #
+    # longitude_difference = longitude_2 - longitude_1
+    # latitude_difference = latitude_2 - latitude_1
+    #
+    # a = sin(latitude_difference / 2) ** 2 + cos(input_latitude_1) * cos(input_latitude_2) * sin(longitude_difference / 2) ** 2
+    # c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    #
+    # return R * c
+    return distance.distance((input_latitude_1,input_longitide_1),(input_latitude_2,input_longitide_2))
 
 
 class HeuristicRouter:
@@ -27,8 +52,8 @@ class HeuristicRouter:
         else:
             self.routes.append(Route(start=self.locations[0],
                                      end=self.locations[-1],
-                                     intermediate_points=[location for i,location in enumerate(self.locations) if
-                                                          i not in [0, len(self.locations)-1]]))
+                                     intermediate_points=[location for i, location in enumerate(self.locations) if
+                                                          i not in [0, len(self.locations) - 1]]))
 
     def calculate_number_of_routes(self, number_of_routes):
         # Call calculate_route a number_of_routes, either sequentially or using threads
@@ -39,19 +64,23 @@ class HeuristicRouter:
         if route_index > len(self.routes):
             return 'Index out of bounds'
         else:
+            print(calculate_distance(self.routes[route_index].start.x,
+                                     self.routes[route_index].start.y,
+                                     self.routes[route_index].end.x,
+                                     self.routes[route_index].end.y))
             if len(self.routes[route_index].intermediate_points) == 0:
-                return 'https://maps.openrouteservice.org/directions?n1={x_start}&n2={y_start}&n3=12&a={x_start},{y_start},{x_end},{y_end}&b=0&c=0&k1=en-US&k2=km'.format(
+                return 'https://maps.openrouteservice.org/directions?n1={y_start}&n2={x_start}&n3=12&a={y_start},{x_start},{y_end},{x_end}&b=0&c=0&k1=en-US&k2=km'.format(
                     x_start=self.routes[route_index].start.x,
                     y_start=self.routes[route_index].start.y,
                     x_end=self.routes[route_index].end.x,
                     y_end=self.routes[route_index].end.y)
             else:
                 return_string = ''
-                return_string += 'https://maps.openrouteservice.org/directions?n1={x_start}&n2={y_start}&n3=12&a={x_start},{y_start}'.format(
+                return_string += 'https://maps.openrouteservice.org/directions?n1={y_start}&n2={x_start}&n3=12&a={y_start},{x_start}'.format(
                     x_start=self.routes[route_index].start.x,
                     y_start=self.routes[route_index].start.y)
                 for point in self.routes[route_index].intermediate_points:
-                    return_string += ',{x_point},{y_point}'.format(x_point=point.x,
+                    return_string += ',{y_point},{x_point}'.format(x_point=point.x,
                                                                    y_point=point.y)
 
                 return_string += ',{x_end},{y_end}&b=0&c=0&k1=en-US&k2=km'.format(
--- SPDB/main.py
+++ SPDB/main.py
@@ -1,12 +1,13 @@
 import random
 from HeuristicRouter import HeuristicRouter, Location, Route
 
+
 def main():
     router = HeuristicRouter()
 
-    for i in range(10):
-        router.add_location(Location(longitude=random.randrange(5211,5231)/100,
-                                     latitude=random.randrange(2084,2125)/100))
+    for i in range(2):
+        router.add_location(Location(latitude=random.randrange(5211, 5231) / 100,
+                                     longitude=random.randrange(2084, 2125) / 100))
 
     router.calculate_number_of_routes(1)
     print(router.export_route_to_link(0))
@@ -14,4 +15,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
