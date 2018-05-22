2eaee3c5e5d46832de8d73e5e2b2cdf594639267
Krecik <Marcin.Jarzebski94@gmail.com>
2018-05-22 06:19:58 +0200

    working route finding logic

--- SPDB/HeuristicRouter.py
+++ SPDB/HeuristicRouter.py
@@ -1,6 +1,6 @@
 from math import sin, cos, sqrt, atan2, radians, degrees
 from geopy import distance
-import copy
+import random
 
 
 class Route:
@@ -21,11 +21,36 @@ class Location:
         self.node_id = node_id
 
 
-def calculate_distance(input_longitide_1,
+class RoutingDatapoint(Location):
+    def __init__(self, location, longitude, latitude, xml_data=None, node_id=0):
+        super().__init__(longitude, latitude, xml_data, node_id)
+        self.corresponding_location = location
+        self.distances = {}
+
+        self.current_distance = 0
+        self.distance_weight = 0
+
+        self.current_angle = 0
+        self.bearing_weight = 0
+
+        self.clump_score = 0
+        self.clump_weight = 0
+
+        self.random_value = 0
+        self.random_weight = 0
+
+        self.total_weight = 0
+
+    def recalculate_total_weight(self, distance_multiplier=1, bearing_multiplier=1, clump_multiplier=1,
+                                 random_multiplier=1):
+        self.total_weight = self.distance_weight * distance_multiplier + self.bearing_weight * bearing_multiplier + self.clump_weight * clump_multiplier + self.random_weight * random_multiplier
+
+
+def calculate_distance(input_longitude_1,
                        input_latitude_1,
-                       input_longitide_2,
+                       input_longitude_2,
                        input_latitude_2):
-    return distance.distance((input_latitude_1, input_longitide_1), (input_latitude_2, input_longitide_2)).km
+    return distance.distance((input_latitude_1, input_longitude_1), (input_latitude_2, input_longitude_2)).km
 
 
 def calculate_bearing_difference(b1, b2):
@@ -73,12 +98,13 @@ def calculate_initial_compass_bearing(pointA, pointB):
 
 
 class HeuristicRouter:
-    def __init__(self, initial_locations=None):
+    def __init__(self, initial_locations=None, max_distance=100):
         if initial_locations is not None:
             self.locations = initial_locations
         else:
             self.locations = []
         self.routes = []
+        self.max_distance = max_distance
 
     def add_location(self, location):
         self.locations.append(location)
@@ -86,14 +112,106 @@ class HeuristicRouter:
     def _calculate_route(self):
         if len(self.locations) < 2:
             raise Exception('Not enough locations')
-        else:
+        elif len(self.locations) == 2:
             self.routes.append(Route(start=self.locations[0],
-                                     end=self.locations[-1],
-                                     intermediate_points=[location for i, location in enumerate(self.locations) if
-                                                          i not in [0, len(self.locations) - 1]]))
+                                     end=self.locations[-1]))
+        else:
 
             # TODO: ROUTE CALCULATOR GOES HERE
-            locations = copy.deepcopy(self.locations)
+            intermediate_points = [location for i, location in enumerate(self.locations) if
+                                   i not in [0, len(self.locations) - 1]]
+            datapoints = []
+            for point in intermediate_points:
+                datapoints.append(RoutingDatapoint(point, point.x, point.y))
+
+            for datapoint in datapoints:
+                for distance_to in self.locations:
+                    datapoint.distances[distance_to] = calculate_distance(datapoint.x,
+                                                                          datapoint.y,
+                                                                          distance_to.x,
+                                                                          distance_to.y)
+
+            for datapoint in datapoints:
+                datapoint.current_distance = datapoint.distances[self.locations[0]]
+
+            for datapoint in datapoints:
+                datapoint.current_angle = calculate_bearing_difference(
+                    calculate_initial_compass_bearing((self.locations[0].y, self.locations[0].x),
+                                                      (self.locations[-1].y, self.locations[-1].x)),
+                    calculate_initial_compass_bearing((self.locations[0].y, self.locations[0].x),
+                                                      (datapoint.y, datapoint.x)))
+
+            for datapoint in datapoints:
+                sorted_distances = sorted(list(datapoint.distances.values()))
+                NUMBER_OF_POINTS_CALCULATED_INTO_CLUMP = 4
+                datapoint.clump_score = sum(sorted_distances[0:NUMBER_OF_POINTS_CALCULATED_INTO_CLUMP])
+
+            for datapoint in datapoints:
+                datapoint.random_value = random.randrange(0, 1000)
+
+            datapoints = sorted(datapoints, key=lambda datapoint: datapoint.current_distance)
+            for i in range(len(datapoints)):
+                datapoints[i].distance_weight = len(datapoints) - i
+
+            datapoints = sorted(datapoints, key=lambda datapoint: datapoint.current_angle)
+            for i in range(len(datapoints)):
+                datapoints[i].bearing_weight = len(datapoints) - i
+
+            datapoints = sorted(datapoints, key=lambda datapoint: datapoint.clump_score)
+            for i in range(len(datapoints)):
+                datapoints[i].clump_weight = len(datapoints) - i
+
+            datapoints = sorted(datapoints, key=lambda datapoint: datapoint.random_value)
+            for i in range(len(datapoints)):
+                datapoints[i].random_weight = len(datapoints) - i
+
+            for i in range(len(datapoints)):
+                datapoints[i].recalculate_total_weight(1, 1, 1, 1)
+
+            datapoints = sorted(datapoints, key=lambda datapoint: datapoint.total_weight, reverse=True)
+
+            accumulated_distance = 0
+            constructed_route = []
+
+            constructed_route.append(datapoints.pop(0))
+            accumulated_distance += constructed_route[0].distances[self.locations[0]]
+
+            while len(datapoints) > 0 and accumulated_distance < self.max_distance:
+                constructed_route.append(datapoints.pop(0))
+                accumulated_distance += constructed_route[-1].distances[constructed_route[-2].corresponding_location]
+                if accumulated_distance > self.max_distance:
+                    constructed_route.pop()
+                    break
+
+                for datapoint in datapoints:
+                    datapoint.current_distance = datapoint.distances[constructed_route[-1].corresponding_location]
+                datapoints = sorted(datapoints, key=lambda datapoint: datapoint.current_distance)
+                for i in range(len(datapoints)):
+                    datapoints[i].distance_weight = len(datapoints) - i
+
+                for datapoint in datapoints:
+                    datapoint.current_angle = calculate_bearing_difference(
+                        calculate_initial_compass_bearing((constructed_route[-1].y, constructed_route[-1].x),
+                                                          (self.locations[-1].y, self.locations[-1].x)),
+                        calculate_initial_compass_bearing((constructed_route[-1].y, constructed_route[-1].x),
+                                                          (datapoint.y, datapoint.x)))
+                datapoints = sorted(datapoints, key=lambda datapoint: datapoint.current_angle)
+                for i in range(len(datapoints)):
+                    datapoints[i].bearing_weight = len(datapoints) - i
+
+                for i in range(len(datapoints)):
+                    datapoints[i].recalculate_total_weight(1, 1, 1, 1)
+
+                datapoints = sorted(datapoints, key=lambda datapoint: datapoint.total_weight, reverse=True)
+
+            if len(self.routes) == 0 or len(self.routes[-1].intermediate_points) > len(constructed_route):
+                print('found_better')
+                self.routes.append(Route(start=self.locations[0],
+                                         end=self.locations[-1],
+                                         intermediate_points=[route_point.corresponding_location for route_point in
+                                                              constructed_route]))
+
+            pass
 
     def calculate_number_of_routes(self, number_of_routes):
         # Call calculate_route a number_of_routes, either sequentially or using threads
@@ -101,9 +219,24 @@ class HeuristicRouter:
             self._calculate_route()
 
     def export_route_to_link(self, route_index):
+        all_points_link = ''
+        all_points_link += 'https://maps.openrouteservice.org/directions?n1={y_start}&n2={x_start}&n3=12&a={y_start},{x_start}'.format(
+            x_start=self.locations[0].x,
+            y_start=self.locations[0].y)
+        for point in [location for i, location in enumerate(self.locations) if
+                      i not in [0, len(self.locations) - 1]]:
+            all_points_link += ',{y_point},{x_point}'.format(x_point=point.x,
+                                                             y_point=point.y)
+
+        all_points_link += ',{y_end},{x_end}&b=0&c=0&k1=en-US&k2=km'.format(
+            x_end=self.locations[-1].x,
+            y_end=self.locations[-1].y)
+        print('All points: {link}'.format(link=all_points_link))
+
         if route_index > len(self.routes):
             return 'Index out of bounds'
         else:
+            # DEBUG PRINTS START
             print(calculate_distance(self.routes[route_index].start.x,
                                      self.routes[route_index].start.y,
                                      self.routes[route_index].end.x,
@@ -117,7 +250,9 @@ class HeuristicRouter:
                 (self.routes[route_index].intermediate_points[0].y,
                  self.routes[route_index].intermediate_points[0].x))
             print(bearing_2)
-            print(calculate_bearing_difference(bearing_1,bearing_2))
+            print(calculate_bearing_difference(bearing_1, bearing_2))
+            # DEBUG PRINTS END
+
             if len(self.routes[route_index].intermediate_points) == 0:
                 return 'https://maps.openrouteservice.org/directions?n1={y_start}&n2={x_start}&n3=12&a={y_start},{x_start},{y_end},{x_end}&b=0&c=0&k1=en-US&k2=km'.format(
                     x_start=self.routes[route_index].start.x,
--- SPDB/main.py
+++ SPDB/main.py
@@ -3,13 +3,18 @@ from HeuristicRouter import HeuristicRouter, Location, Route
 
 
 def main():
-    router = HeuristicRouter()
+    router = HeuristicRouter(max_distance=100)
 
-    for i in range(3):
+    router.add_location(Location(latitude=52.05,
+                                 longitude=20.5))
+    for i in range(14):
         router.add_location(Location(latitude=random.randrange(5211, 5231) / 100,
                                      longitude=random.randrange(2084, 2125) / 100))
 
-    router.calculate_number_of_routes(1)
+    router.add_location(Location(latitude=52.4,
+                                 longitude=21.3))
+
+    router.calculate_number_of_routes(200)
     print(router.export_route_to_link(0))
 
 
