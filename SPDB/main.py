c63db21a991a5bafbc1c69b70dc9335dd0f6358d
wolframowy <wolframowy@o2.pl>
2018-06-07 22:37:58 +0200

    Add openrouteservice api for calculating route length and travel time

--- SPDB/main.py
+++ SPDB/main.py
@@ -1,18 +1,32 @@
 import random
 from HeuristicRouter import HeuristicRouter, Location, Route
+from openrouteservice import client
 
+api_key = '58d904a497c67e00015b45fc1f9700c9ad4d40e1b7e22af323c7dbbe'
+start = {'latitude': 52.05, 'longitude': 20.5}
+end = {'latitude': 52.4, 'longitude': 21.3}
 
 def main():
+    clnt = client.Client(key=api_key)
     router = HeuristicRouter(max_distance=80)
 
-    router.add_location(Location(latitude=52.05,
-                                 longitude=20.5))
+    router.add_location(Location(latitude=start['latitude'],
+                                 longitude=start['longitude']))
     for i in range(14):
         router.add_location(Location(latitude=random.randrange(5211, 5231) / 100,
                                      longitude=random.randrange(2084, 2125) / 100))
 
-    router.add_location(Location(latitude=52.4,
-                                 longitude=21.3))
+    router.add_location(Location(latitude=end['latitude'],
+                                 longitude=end['longitude']))
+
+    params_route = {'profile': 'driving-car', 'format_out': 'json', 'units': 'km', 'geometry': 'false',
+                    'geometry_format': 'geojson', 'instructions': 'false',
+                    'coordinates': [[start['longitude'], start['latitude']],
+                                   [end['longitude'], end['latitude']]
+                                   ]}
+
+    route = clnt.directions(**params_route)
+    print(route)
 
     router.calculate_number_of_routes(200)
     print(router.export_route_to_link(-1))
