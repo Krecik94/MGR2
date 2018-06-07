2efbb10b0fcf4b369e9b9f6dd5f7acde1108a6ed
Krecik <Marcin.Jarzebski94@gmail.com>
2018-06-07 22:41:22 +0200

    print interation number

--- SPDB/main.py
+++ SPDB/main.py
@@ -1,20 +1,21 @@
 import random
 from HeuristicRouter import HeuristicRouter, Location, Route
 
-
 def main():
     router = HeuristicRouter(max_distance=80)
 
     router.add_location(Location(latitude=52.05,
                                  longitude=20.5))
-    for i in range(14):
+    for i in range(200):
         router.add_location(Location(latitude=random.randrange(5211, 5231) / 100,
                                      longitude=random.randrange(2084, 2125) / 100))
 
     router.add_location(Location(latitude=52.4,
                                  longitude=21.3))
 
-    router.calculate_routes_for_given_time()
+
+    iterations = router.calculate_routes_for_given_time()
+    print('Number of iterations: {iterations}'.format(iterations=iterations))
     print(router.export_route_to_link(-1))
 
 
