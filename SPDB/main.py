b1281615236e9282a9244b17b5bb015f8a1052b4
wolframowy <wolframowy@o2.pl>
2018-06-08 00:14:33 +0200

    Chunk intermidiate points for routing and draw route on map

--- SPDB/main.py
+++ SPDB/main.py
@@ -51,18 +51,32 @@ def main():
     print(len(router.get_route(-1).intermediate_points))
 
     best_route = router.get_route(-1)
-    new_route['coordinates'].append([best_route.start.y, best_route.start.x])
+    new_route['coordinates'].append([best_route.start.x, best_route.start.y])
     folium.Marker([best_route.start.y, best_route.start.x]).add_to(map1)
     for loc in best_route.intermediate_points:
-        new_route['coordinates'].append([loc.y, loc.x])
+        new_route['coordinates'].append([loc.x, loc.y])
         folium.Marker([loc.y, loc.x]).add_to(map1)
-    new_route['coordinates'].append([best_route.end.y, best_route.end.x])
+    new_route['coordinates'].append([best_route.end.x, best_route.end.y])
     folium.Marker([best_route.end.y, best_route.end.x]).add_to(map1)
 
-    #route = clnt.directions(**new_route)
-    #folium.features.GeoJson(route).add_to(map1)
+    route_chunked = list(chunks(new_route['coordinates'], 49))
+    mutual_point = []
+    for chunk in route_chunked:
+        new_route['coordinates'] = chunk
+        if(len(mutual_point) != 0):
+            new_route['coordinates'].insert(0, mutual_point)
+        route = clnt.directions(**new_route)
+        folium.features.GeoJson(route).add_to(map1)
+        mutual_point = new_route['coordinates'].pop()
+
+    # route = clnt.directions(**orig_route)
+    # folium.features.GeoJson(route).add_to(map1)
 
     map1.save('map.html')
+
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
 
 if __name__ == '__main__':
     main()
