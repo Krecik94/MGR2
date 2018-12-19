98ebe4c108a553e4f31b31906b5738ae84ae1f2e
Krecik <Marcin.Jarzebski94@gmail.com>
2018-12-20 00:46:54 +0100

    leftover trash

---
+++ test_server.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+Very simple HTTP server in python.
+Usage::
+    ./dummy-web-server.py [<port>]
+Send a GET request::
+    curl http://localhost
+Send a HEAD request::
+    curl -I http://localhost
+Send a POST request::
+    curl -d "foo=bar&bin=baz" http://localhost
+"""
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        print(self.path)
+        self._set_headers()
+        print('tst')
+        self.wfile.write("<html><body><h1>hi!</h1></body></html>".encode('utf-8'))
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+
+
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    'Starting httpd...'
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
